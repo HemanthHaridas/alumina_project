01b2b89c5d8d0317a30d06fbcb749bc100e808e2
Maxime <m.pouvreau@wsu.edu>
2024-09-11 16:01:04 -0700

    Changed

--- test.sh
+++ test.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
+#	lmp="/Users/hemanthharidas/Desktop/Codes/lammps_test/lammps/build/lmp"
+#lmp="lmp"
+lmp="lmp_serial"
+#rm all.inp 2> /dev/null
+t1=$(date +%s%3N)
 for inp in *.inp
 do
 #	echo $inp 
 #	outfile=$(awk '{print substr($0, 1, length($0)-4)}' <<< "$inp")
-#	lmp="/Users/hemanthharidas/Desktop/Codes/lammps_test/lammps/build/lmp"
 #	$lmp -i $inp > $outfile.log
 
-  lmp="lmp"
   prefix=${inp%%.*}
-  t1=$(date +%s%3N)
-  mpirun $lmp -i $inp > $prefix.log 2> >(grep -v "PMIX ERROR" >&2)
-  t2=$(date +%s%3N)
-	dt=$(awk -v t1=$t1 -v t2=$t2 'BEGIN{print (t2-t1)/1000.}')
-	echo "$prefix $dt"
-	dtlist+="$dt "
+  #echo "log $prefix.log" >> all.inp
+	#cat $inp >> all.inp
+	#echo "clear" >> all.inp
+	##-- separate input files: ---
+	echo "$prefix"
+  $lmp -i $inp > $prefix.log 
+#	echo "$prefix $dt";	dtlist+="$dt "
 done
 echo -n "Total time: "
-echo $dtlist | awk '{for(i=1; i<=NF; i++) sum+=$i; print sum}'
+t2=$(date +%s%3N);dt=$(awk -v t1=$t1 -v t2=$t2 'BEGIN{print (t2-t1)/1000.}')
+echo $dt
+#echo $dtlist | awk '{for(i=1; i<=NF; i++) sum+=$i; print sum}'t1=$(date +%s%3N)
+
+#	2> >(grep -v "PMIX ERROR" >&2)
+#t1=$(date +%s%3N)
+#$lmp -i all.inp > all.log
+#t2=$(date +%s%3N)
+#awk -v t1=$t1 -v t2=$t2 'BEGIN{print (t2-t1)/1000.}' 
